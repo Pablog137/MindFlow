9ff0069b8b87802a1a728ad854b526c18fad573b
Pablo <pablogutierrezvaldin@gmail.com>
2024-03-24 13:57:20 +0100

    tailwind setup initial config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
-
-
-function App() {
-
-  return (
-    <h1>
-     Prueba
-    </h1>
-  )
+function MyButton({ title }: { title: string }) {
+    return <button>{title}</button>;
 }
 
-export default App
+export default function MyApp() {
+    return (
+        <div>
+            <h1>Welcome to my app</h1>
+            <MyButton title="I'm a button" />
+        </div>
+    );
+}
