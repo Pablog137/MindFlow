08869857be7786004f315e39c1bfc3e5721d51cd
Pablo <pablogutierrezvaldin@gmail.com>
2024-03-30 11:49:54 +0100

    Login validation

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,17 @@
 import logo from "../assets/img/logo-64.png";
+import useForm from "../hooks/useForm";
+import { useState } from "react";
 
 export default function Login() {
+    const { errors, handleChange, handleSubmit } = useForm();
+    const [showError, setShowError] = useState(false);
+    const errorMessage = errors.email || errors.password;
+
+    const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setShowError(true);
+        handleSubmit(e);
+    };
     return (
         <div className="flex justify-center items-center min-h-screen">
             <div className="text-white grid place-items-center px-6">
@@ -8,9 +19,17 @@ export default function Login() {
                 <h1 className="text-3xl text-center font-bold py-8 md:p-10">
                     Log in to your account
                 </h1>
+                {errorMessage && showError && (
+                    <div className="w-full text-red-400 text-sm text-center p-3 mb-5 rounded border border-red-900 bg-red-950">
+                        {errorMessage}
+                    </div>
+                )}
+
                 <form
                     action=""
-                    className="grid grid-cols-12 gap-4 w-full max-w-md"
+                    onSubmit={submitForm}
+                    className="grid grid-cols-12 gap-4 w-full max-w-md "
+                    noValidate
                 >
                     <div className="col-span-12">
                         <label htmlFor="email" className="font-bold">
@@ -19,8 +38,10 @@ export default function Login() {
                         <input
                             type="email"
                             id="email"
+                            name="email"
                             placeholder="Email"
                             className="bg-gray-800 p-2 rounded w-full mt-2"
+                            onChange={handleChange}
                         />
                     </div>
                     <div className="col-span-12">
@@ -41,8 +62,10 @@ export default function Login() {
                         <input
                             type="password"
                             id="password"
+                            name="password"
                             placeholder="Password"
                             className="bg-gray-800 p-2 rounded w-full mt-2"
+                            onChange={handleChange}
                         />
                     </div>
                     <div className="col-span-12 flex justify-center p-0 py-2">
