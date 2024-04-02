adee19c77fba8c4dca4ae662f12c7d6330e6c38a
Pablo <pablogutierrezvaldin@gmail.com>
2024-04-02 20:28:53 +0200

    Button add a task and enhacement general styles list col

--- frontend/src/components/TodoList/TodoList.tsx
+++ frontend/src/components/TodoList/TodoList.tsx
@@ -1,10 +1,18 @@
 import TodoListItem from "./TodoListItem";
-
 export default function TodoList({ status }) {
     return (
-        <div className="col-span-12 lg:col-span-4 bg-[#EBECF0] text-black rounded-lg p-6 border-2 border-purple-500">
+        <div className="col-span-12 lg:col-span-4 bg-[#EBECF0] text-black rounded-lg p-6 border-2 border-purple-500 flex flex-col ">
             <h2 className="text-xl text-center font-bold pb-3">{status}</h2>
-            <TodoListItem />
+            <div className="flex flex-col gap-2 flex-grow">
+                <TodoListItem />
+                <TodoListItem />
+            </div>
+            <div className="flex justify-center">
+                <button className="bg-purple-500 hover:bg-purple-700 text-white font-semibold px-4 py-2 rounded-md flex justify-center items-center gap-4">
+                    <i className="fa-solid fa-plus"></i>
+                    <p>Add a task</p>
+                </button>
+            </div>
         </div>
     );
 }
