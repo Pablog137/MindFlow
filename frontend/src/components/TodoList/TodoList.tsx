38920522eaf9cdbef97ebef2d6c6f29d22528547
Pablo <pablogutierrezvaldin@gmail.com>
2024-04-02 20:17:00 +0200

    Todo list responsive

--- frontend/src/components/TodoList/TodoList.tsx
+++ frontend/src/components/TodoList/TodoList.tsx
@@ -2,7 +2,7 @@ import TodoListItem from "./TodoListItem";
 
 export default function TodoList({ status }) {
     return (
-        <div className="col-span-4 bg-[#EBECF0] text-black rounded-lg p-6 border-2 border-purple-500">
+        <div className="col-span-12 lg:col-span-4 bg-[#EBECF0] text-black rounded-lg p-6 border-2 border-purple-500">
             <h2 className="text-xl text-center font-bold pb-3">{status}</h2>
             <TodoListItem />
         </div>
--- frontend/src/components/TodoList/TodoListItem.tsx
+++ frontend/src/components/TodoList/TodoListItem.tsx
@@ -1,10 +1,10 @@
 
 export default function TodoListItem() {
   return (
-      <div className="p-4 bg-white rounded-lg">
+      <div className="p-6 bg-white rounded-lg">
           <span className="font-bold text-orange-400">MEDIUM</span>
-          <p className="text-lg">Create final proyect</p>
-          <div className="flex items-center">
+          <p className="text-xl py-6">Create final proyect</p>
+          <div className="flex items-center justify-between">
               <i className="fa-regular fa-clock text-purple-300"></i>
               <span className="text-sm text-gray-500">15 september</span>
               <i className="fa-solid fa-trash text-red-500"></i>
