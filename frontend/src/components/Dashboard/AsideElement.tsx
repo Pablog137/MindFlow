fd977d707e1b5e6e5ec6c4cef84e223e002a754c
Pablo <pablogutierrezvaldin@gmail.com>
2024-04-02 19:08:22 +0200

    Custom text and icons aside bar dashboard

---
+++ frontend/src/components/Dashboard/AsideElement.tsx
@@ -0,0 +1,20 @@
+export default function AsideElement({ text, icon }) {
+    return (
+        <li className="p-2">
+            <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+            >
+                <i
+                    className={
+                        icon +
+                        " text-xl md:text-2xl text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    }
+                ></i>
+                <span className="flex-1 whitespace-nowrap ms-3 hidden lg:flex">
+                    {text}
+                </span>
+            </a>
+        </li>
+    );
+}
