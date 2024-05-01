d53b1cebbe441c4795fb28a7b4ed361f170c6b6c
Pablo <pablogutierrezvaldin@gmail.com>
2024-05-01 10:38:37 +0200

    command fixed

--- frontend/src/components/NotesEditor/Command.tsx
+++ frontend/src/components/NotesEditor/Command.tsx
@@ -28,14 +28,14 @@ export default function Command({
     };
     return (
         <li>
-            <a
+            <button
                 onClick={handleClick}
                 className={`p-2 lg:p-3 font-serif font-bold hover:bg-gray-200 hover:rounded-md ${
                     isSelected ? "bg-gray-400 rounded-md hover:bg-gray-400" : ""
                 }`}
             >
                 {content}
-            </a>
+            </button>
         </li>
     );
 }
